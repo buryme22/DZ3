b348bcf620ab09a8ac366d51c29db2442131d5c6
Alexey <redy22@mail.ru>
2022-12-02 14:31:54 +0400

    new

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num/10000;
+int num2 = num%10000/1000;
+int num3 = num%100/10;
+int num4 = num%10;
+
+if (num1 == num4 && num2 == num3) {
+    Console.WriteLine($"Число является палиндромом");
+}
+else Console.WriteLine($"Число не палиндром");
+
+
